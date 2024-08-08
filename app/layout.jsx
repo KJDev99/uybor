c6b4259d4ddfd7593499b1e979f1401debdeeaca
jamshidqayimov <jamshidqayimov0399@gmail.com>
2024-08-08 21:33:46 +0500

    push

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
+import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import "@/assets/styles/globals.css";
 import Footer from "@/components/Footer";
 import ClientProvider from "@/components/ClientProvider";
+
 export const metadata = {
   title: "TOPUY, Ko'chmas mulklarning katta bozori ",
   description: "Ko'chmas mulklarning katta bozori ",
@@ -12,6 +14,45 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta
+          name="google-site-verification"
+          content="UkR4dh55Cj77k3gf1-90XU77TKTWqS6iGqR-z1wC7Tw"
+        />
+        {/* Yandex.Metrika counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(98023827, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true,
+                   ecommerce:"dataLayer"
+              });
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/98023827"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+      </Head>
       <body className="bg-background">
         <ClientProvider>
           <Navbar />
