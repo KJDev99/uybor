cef51421d850c222eb7b796d4dc97c0db73ea0c5
jamshidqayimov <jamshidqayimov0399@gmail.com>
2024-10-22 17:21:24 +0500

    referal

--- app/page.jsx
+++ app/page.jsx
@@ -5,14 +5,23 @@ import Hero from "@/components/Hero";
 import React, { useEffect, useState } from "react";
 import InnerBg from "@/components/InnerBg";
 import Tavfsiya from "@/components/Tavfsiya";
+import { useSearchParams } from "next/navigation";
 
 const Page = (params) => {
   const searchParams = params.searchParams.search || null;
   const [count, setCount] = useState();
 
   useEffect(() => {
-    sessionStorage.setItem("referal", params.searchParams.search);
-  }, [searchParams]);
+    if (typeof window !== "undefined") {
+      const params = new URLSearchParams(window.location.search);
+      const entries = [...params.entries()]; // Barcha parametrlardan massiv yaratamiz
+
+      if (entries.length > 0) {
+        sessionStorage.setItem("referal", entries[0][0]);
+      }
+    }
+  }, []);
+
   useEffect(() => {
     // Google Tag Manager and Analytics initialization
     const gtagScript = document.createElement("script");
